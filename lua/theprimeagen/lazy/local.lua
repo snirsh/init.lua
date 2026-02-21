12b104f296d13ae41a3e06b67edd1aaa681bc2b4
snirsh <s.sharristh@gmail.com>
2026-02-21 11:24:50 +0200

    fix nvim-treesitter v1 API and disable missing rfceez plugin
    nvim-treesitter rewrote its API on main branch, removing nvim-treesitter.configs.
    Update config to use the new v1 API: install parsers via require("nvim-treesitter").install()
    and enable highlight via a FileType autocmd with vim.treesitter.start().
    Also comment out rfceez local plugin since ~/personal/rfceez does not exist.

--- lua/theprimeagen/lazy/local.lua
+++ lua/theprimeagen/lazy/local.lua
@@ -1,18 +1,18 @@
 
 local local_plugins = {
-    {
-        "rfceez",
-        dir = "~/personal/rfceez",
-        config = function()
-            local rfc = require("rfceez")
-            rfc.setup()
-            vim.keymap.set("n", "<leader>ra", function() rfc.add() end)
-            vim.keymap.set("n", "<leader>rd", function() rfc.rm() end)
-            vim.keymap.set("n", "<leader>rs", function() rfc.show_notes() end)
-            vim.keymap.set("n", "[r", function() rfc.nav_next() end)
-            vim.keymap.set("n", "[[r", function() rfc.show_next() end)
-        end
-    },
+    -- {
+    --     "rfceez",
+    --     dir = "~/personal/rfceez",
+    --     config = function()
+    --         local rfc = require("rfceez")
+    --         rfc.setup()
+    --         vim.keymap.set("n", "<leader>ra", function() rfc.add() end)
+    --         vim.keymap.set("n", "<leader>rd", function() rfc.rm() end)
+    --         vim.keymap.set("n", "<leader>rs", function() rfc.show_notes() end)
+    --         vim.keymap.set("n", "[r", function() rfc.nav_next() end)
+    --         vim.keymap.set("n", "[[r", function() rfc.show_next() end)
+    --     end
+    -- },
     {
         "ThePrimeagen/harpoon",
         branch = "harpoon2",
--- lua/theprimeagen/lazy/treesitter.lua
+++ lua/theprimeagen/lazy/treesitter.lua
@@ -1,43 +1,30 @@
 return {
     "nvim-treesitter/nvim-treesitter",
+    lazy = false, -- v1 does not support lazy-loading
     build = ":TSUpdate",
     config = function()
-        require("nvim-treesitter.configs").setup({
-            -- A list of parser names, or "all"
-            ensure_installed = {
-                "vimdoc", "javascript", "typescript", "tsx", "c", "lua", "rust",
-                "jsdoc", "bash", "json", "html", "css",
-            },
+        -- nvim-treesitter v1 rewrote the API: nvim-treesitter.configs is gone.
+        -- Highlight/indent are now built-in Neovim features enabled via autocmd.
 
-            -- Install parsers synchronously (only applied to `ensure_installed`)
-            sync_install = false,
-
-            -- Automatically install missing parsers when entering buffer
-            -- Recommendation: set to false if you don"t have `tree-sitter` CLI installed locally
-            auto_install = true,
-
-            indent = {
-                enable = true
-            },
-
-            highlight = {
-                -- `false` will disable the whole extension
-                enable = true,
-
-                -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-                -- Set this to `true` if you depend on "syntax" being enabled (like for indentation).
-                -- Using this option may slow down your editor, and you may see some duplicate highlights.
-                -- Instead of true it can also be a list of languages
-                additional_vim_regex_highlighting = { "markdown" },
-            },
+        -- Install parsers (no-op if already installed)
+        require("nvim-treesitter").install({
+            "vimdoc", "javascript", "typescript", "tsx", "c", "lua", "rust",
+            "jsdoc", "bash", "json", "html", "css",
         })
 
-        local treesitter_parser_config = require("nvim-treesitter.parsers").get_parser_configs()
-        treesitter_parser_config.templ = {
+        -- Enable treesitter highlighting for all filetypes
+        vim.api.nvim_create_autocmd("FileType", {
+            callback = function()
+                pcall(vim.treesitter.start)
+            end,
+        })
+
+        -- Add custom templ parser
+        local parsers = require("nvim-treesitter.parsers")
+        parsers.templ = {
             install_info = {
                 url = "https://github.com/vrischmann/tree-sitter-templ.git",
-                files = {"src/parser.c", "src/scanner.c"},
-                branch = "master",
+                files = { "src/parser.c", "src/scanner.c" },
             },
         }
 
